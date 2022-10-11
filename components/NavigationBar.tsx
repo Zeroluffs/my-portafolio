3916c042a5454f31b56df72a896c3324f86d98fb
zeroluffs <zeroluffs@gmail.com>
2022-10-11 17:25:08 -0500

    list of nav items

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,33 +1,68 @@
 import Link from "next/link";
 import pdf from "../public/assets/JhonerPineda_CV.pdf";
 
+interface NavLinkType {
+  number: number;
+  name: string;
+}
+
+const navigationRoutes = [
+  {
+    id: 0,
+    number: 1,
+    name: "About",
+  },
+  {
+    id: 1,
+    number: 2,
+    name: "Experience",
+  },
+  {
+    id: 2,
+    number: 3,
+    name: "Work",
+  },
+  {
+    id: 3,
+    number: 4,
+    name: "Contact",
+  },
+];
+
 export function NavigationBar() {
-  const onClick = () => {
-    window.open(pdf);
-  };
   return (
     <nav>
       <div className="flex flex-col justify-between pt-8 mx-4 md:flex-row lg:flex-row lg:pt-14">
-        {/* <Link href={pdf} >
-          <a className="px-6 py-2 border-2 rounded-sm text-leaf hover:bg-slate-600 hover:bg-opacity-20 border-leaf">
+        <div className="text-4xl text-lightState">JP</div>
+        <ul className="flex flex-row justify-center gap-4">
+          {navigationRoutes.map((singleRoute) => {
+            return (
+              <NavigationLinks
+                key={singleRoute.id}
+                number={singleRoute.number}
+                name={singleRoute.name}
+              />
+            );
+          })}
+          <a
+            href={"assets/JhonerPineda_CV.pdf"}
+            target="_blank"
+            rel="noreferrer"
+            className="px-6 py-2 pb-2 -my-1 border-2 rounded-sm text-leaf hover:bg-slate-600 hover:bg-opacity-20 border-leaf"
+          >
             Resume
           </a>
-        </Link> */}
-        <a
-          href={"assets/JhonerPineda_CV.pdf"}
-          target="_blank"
-          rel="noreferrer"
-          className="px-6 py-2 border-2 rounded-sm text-leaf hover:bg-slate-600 hover:bg-opacity-20 border-leaf"
-        >
-          Resume
-        </a>
-        <button
-          onClick={onClick}
-          className="px-6 py-2 border-2 rounded-sm text-leaf hover:bg-slate-600 hover:bg-opacity-20 border-leaf"
-        >
-          Resume
-        </button>
+        </ul>
       </div>
     </nav>
+  );
+}
+
+function NavigationLinks({ number, name }: NavLinkType) {
+  return (
+    <li className="flex mt-2">
+      <span className="text-leaf "> 0.{number}</span>
+      <p className="ml-4 font-thin text-lightState">{name}</p>
+    </li>
   );
 }
