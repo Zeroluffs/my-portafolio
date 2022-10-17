2441b22395f61ce45818dc972436e2b4f26000c3
zeroluffs <zeroluffs@gmail.com>
2022-10-17 14:33:16 -0500

    experience component

--- components/experience/Experience.tsx
+++ components/experience/Experience.tsx
@@ -13,9 +13,9 @@ export function Experience() {
   const [openTab, setOpenTab] = React.useState(1);
 
   return (
-    <section id="Experience ">
-      <div className="flex flex-row">
-        <div className="pb-4 ml-32 border-l border-gray-700 ">
+    <section id="Experience" className="h-[100vh]">
+      <div className="flex flex-row justify-center">
+        <div className="pb-4 ml-32 border-l border-heig border-gray-700 h-[225px] ">
           <ul className="flex flex-col text-white" role="tablist">
             {companiesWorked.map((company, id) => {
               return (
@@ -31,43 +31,22 @@ export function Experience() {
             })}
           </ul>
         </div>
-
-        <div id="myTabContent">
-          <div
-            className={classNames(
-              openTab === 1 ? "block" : "hidden",
-              " p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            )}
-            id="link1"
-          >
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
-              <strong className="font-medium text-gray-800 dark:text-white">
-                Profile tabs associated content
-              </strong>
-              . Clicking another tab will toggle the visibility of this one for
-              the next. The tab JavaScript swaps classes to control the content
-              visibility and styling.
-            </p>
-          </div>
-          <div
-            className={classNames(
-              openTab === 2 ? "block" : "hidden",
-              " p-4 rounded-lg bg-gray-50 dark:bg-gray-800"
-            )}
-            id="link2"
-          >
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              This is some placeholder content the{" "}
-              <strong className="font-medium text-gray-800 dark:text-white">
-                Dashboard tabs associated content
-              </strong>
-              . Clicking another tab will toggle the visibility of this one for
-              the next. The tab JavaScript swaps classes to control the content
-              visibility and styling.
-            </p>
-          </div>
-        </div>
+        {companiesWorked.map((company, index) => {
+          return (
+            <div key={index}>
+              <CompanyExperience
+                name={company.name}
+                link={company.link}
+                id={company.id}
+                role={company.role}
+                startDate={company.startDate}
+                endDate={company.endDate}
+                thingsWorkedOn={company.thingsWorkedOn}
+                openTab={openTab}
+              />
+            </div>
+          );
+        })}
       </div>
     </section>
   );
@@ -80,11 +59,11 @@ function CompaniesList({ name, id, openTab, setOpenTab }: CompaniesListType) {
       <a
         className={classNames(
           openTab === id ? "border-l-2 border-gray-300" : "",
-          "inline-block p-4  hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+          "inline-block p-4 text-slate-400 transition transition-duration-300ms  hover:text-leaf hover:border-gray-300 "
         )}
         id="profile-tab"
         data-toggle="tab"
-        href="#link1"
+        href={`#link${id}`}
         role="tablist"
         onClick={(e) => {
           e.preventDefault();
@@ -94,5 +73,58 @@ function CompaniesList({ name, id, openTab, setOpenTab }: CompaniesListType) {
         {name}
       </a>
     </li>
+  );
+}
+
+function CompanyExperience({
+  name,
+  link,
+  id,
+  role,
+  startDate,
+  endDate,
+  thingsWorkedOn,
+  openTab,
+}: any) {
+  return (
+    <div id="myTabContent">
+      <div
+        className={classNames(
+          openTab === id ? "block" : "hidden",
+          " p-4 rounded-lg "
+        )}
+        id={`#link${id}`}
+      >
+        <div className="flex flex-col">
+          <p className="text-xl text-lightState">
+            {" "}
+            {role}{" "}
+            <span className="text-leaf">
+              @{" "}
+              <a
+                className="transition-all duration-100 delay-100 border-b-0 hover:border-b-2 border-leaf"
+                target="_blank"
+                rel="noreferrer"
+                href={link}
+              >
+                {name}
+              </a>{" "}
+            </span>
+          </p>
+          <p className="font-extralight text-slate-400">
+            {startDate} - {endDate}
+          </p>
+          <ul className="mt-2 ml-8 list-disc text-slate-400 w-[500px] list-disc-blue marker:text-leaf">
+            {thingsWorkedOn.map((task: string, index: number) => {
+              return (
+                <li className="my-2" key={index}>
+                  {task}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
