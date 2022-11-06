775d84cf5678b86604855868dcd06f75b3338649
zeroluffs <zeroluffs@gmail.com>
2022-11-06 14:45:41 -0500

    button font size

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -9,7 +9,6 @@ export function ContactForm() {
           to hear from you!
         </p>
       </div>
-
       <div className="flex flex-col items-center justify-center mt-20 bg-lightNavy w-[800px] h-[648px] m-auto">
         <form className="flex flex-col">
           <div>
@@ -25,13 +24,13 @@ export function ContactForm() {
             ></input>
           </div>
           <div>
-            <label htmlFor="name" className="text-base text-gray-300">
+            <label htmlFor="email" className="text-base text-gray-300">
               Email Address *
             </label>
             <input
               required
               className="block h-12 p-4 my-4 rounded-md w-[500px] bg-midnight text-lightState "
-              id="name"
+              id="email"
               type="email"
               placeholder="Enter Your Email Address"
             ></input>
@@ -43,12 +42,12 @@ export function ContactForm() {
             <textarea
               required
               placeholder="Enter Your Message"
-              className="block h-48 p-2 my-8 transition w-[500px] ease-in-out rounded-md text-lightState bg-midnight  "
+              className="block h-48 p-4 my-8 transition w-[500px] ease-in-out rounded-md text-lightState bg-midnight  "
               id="name"
             ></textarea>
           </div>
 
-          <button className="mt-4 ml-auto text-2xl transition-colors duration-200 transform border rounded-md h-14 w-52 border-leaf text-leaf bg-midnight hover:bg-leaf hover:bg-opacity-10 focus:bg-leaf">
+          <button className="mt-4 ml-auto text-xl transition-colors duration-200 transform border rounded-md h-14 w-52 border-leaf text-leaf bg-midnight hover:bg-leaf hover:bg-opacity-10 focus:bg-leaf">
             Say Hello
           </button>
         </form>
