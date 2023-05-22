7b70b4239e55fca5ebcdb04ec303e82096b82c6f
zeroluffs <zeroluffs@gmail.com>
2023-05-22 16:14:31 -0500

    url shortener to projects

--- components/work/utils/ProjectsDone.ts
+++ components/work/utils/ProjectsDone.ts
@@ -1,19 +1,26 @@
 export const projectsWorked = [
   {
     id: 1,
-    name: "Budget App",
-    description: "App to keep track of your expenses",
-    techUsed: ["Next.js", "Node", "Express"],
-    gitHub: "https://github.com/Zeroluffs/BudgetApp",
-    link: "https://www.linkedin.com/in/jhonerpineda/",
+    name: 'Budget App',
+    description: 'App to keep track of your expenses',
+    techUsed: ['Next.js', 'Node', 'Express'],
+    gitHub: 'https://github.com/Zeroluffs/BudgetApp',
+    link: 'https://www.linkedin.com/in/jhonerpineda/',
   },
   {
     id: 2,
-    name: "Art Showcase",
-    description:
-      "Portafolio like website for illustrators to showcase their work",
-    techUsed: ["Next.js", "Tailwind", "AWS"],
-    gitHub: "https://github.com/Zeroluffs/art-showcase",
-    link: "https://www.linkedin.com/in/jhonerpineda/",
+    name: 'Art Showcase',
+    description: 'Portafolio like website for illustrators to showcase their work',
+    techUsed: ['Next.js', 'Tailwind', 'AWS'],
+    gitHub: 'https://github.com/Zeroluffs/art-showcase',
+    link: 'https://www.linkedin.com/in/jhonerpineda/',
   },
-];
+  {
+    id: 3,
+    name: 'URL Shortener',
+    description: 'Quickly shorten your links',
+    techUsed: ['Next.js', 'Tailwind', 'React Query'],
+    gitHub: 'https://github.com/Zeroluffs/url-shortener',
+    link: 'https://sparkly-pastelito-0c324c.netlify.app/',
+  },
+]
